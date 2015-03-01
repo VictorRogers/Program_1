31744797ad065026d9ea6ec6af6b56a6ea743535
Victor Rogers <victor.r.rogers@gmail.com>
2015-03-01 12:02:30 -0500

    Basic server functionality working

--- server.c
+++ server.c
@@ -14,8 +14,15 @@
 int main() {
 	struct sockaddr_in sin;
   unsigned int s, new_s, len;
+	int bytes_recv, bytes_sent;
 	char buf[BUFSIZE];
-
+	
+	char status000[] = "000Tell me a joke.";
+	char status100[] = "100Knock Knock."; 
+	char status200[] = "200Who's there?";
+	char status300[] = "300Doughnut.";
+	char status400[] = "400Doughnut who?";
+	char status500[] = "500Doughnut ask, it is a secret.";
 
 	//Address Data Structure
 	bzero(&sin, sizeof(sin));
@@ -26,13 +33,13 @@ int main() {
 	//Open Socket
 	if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
 		perror("Error while creating socket");
-		exit(EXIT_FAILURE);	
+		exit(1);	
 	}
 
 	//Bind socket to address	
 	if ((bind(s, (struct sockaddr *)&sin, sizeof(sin))) < 0) {
 		perror("Error while binding the port");
-		exit(EXIT_FAILURE);	
+		exit(1);	
 	}
 
 	listen(s, MAX_PENDING);
@@ -42,12 +49,36 @@ int main() {
 
 		if ((new_s = accept(s, (struct sockaddr *)&sin, &len)) < 0) {
 			perror("Error while accepting the connection");
-			exit(EXIT_FAILURE);	
+			exit(1);	
 		}
-		
-		if ((bytes = recv(new_s, buf, BUFSIZE-1, 0)) == -1) {
-			perror("Error while receiving data");
-			exit(EXIT_FAILURE);
+
+		while(bytes_recv = recv(new_s, buf, BUFSIZE-1, 0)) {
+			buf[bytes_recv] = '\0';	
+			
+			printf("User sent: %s \n", buf);
+
+			if (strcmp(buf, status000) == 0) { 
+				if ((bytes_sent = send(new_s, status100, strlen(status100), 0)) == -1) {
+					perror("Error while sending data");
+					exit(1);
+				}
+			}	
+
+			else if (strcmp(buf, status200) == 0) {
+				if ((bytes_sent = send(new_s, status300, strlen(status300), 0)) == -1) {
+					perror("Error while sending data");
+					exit(1);
+				}
+			}
+
+			else if (strcmp(buf, status400) ==0) {
+				if ((bytes_sent = send(new_s, status500, strlen(status500), 0)) == -1) {
+					perror("Error while sending data");
+					exit(1);
+				} else {
+					close(new_s);
+				}
+			}
 		}
 	}
 }
